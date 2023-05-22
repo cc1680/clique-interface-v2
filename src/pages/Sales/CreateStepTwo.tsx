a7b4c2032c4309d438cfc3f822f28a229933a66b
CQZ <chenyoulsp@gmail.com>
2023-05-22 19:47:48 +0800

    Create Discount Sales Step Two

--- src/pages/Sales/CreateStepTwo.tsx
+++ src/pages/Sales/CreateStepTwo.tsx
@@ -7,43 +7,40 @@ import {
   MenuItem,
   SelectChangeEvent,
   ToggleButtonGroup,
-  ToggleButton
+  ToggleButton,
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow
 } from '@mui/material'
 import { ReactComponent as StepTwo } from 'assets/svg/steptwo.svg'
+import { ReactComponent as Timeicon } from 'assets/svg/timeicon.svg'
 import Input from 'components/Input'
+import SwitchBtn from 'components/Switch'
+import { useState, ChangeEvent } from 'react'
 
-import { useState } from 'react'
-
-const InputTitle = styled(Typography)`
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  color: #80829f;
-  margin-bottom: 10px;
-  white-space: 'nowrap';
-`
-const Row = styled(Box)(() => ({
-  width: '100%',
-  maxWidth: '866px',
-  display: 'flex',
-  justifyContent: 'space-between'
+const InputTitle = styled(Typography)(() => ({
+  fontFamily: 'Inter',
+  fontStyle: 'normal',
+  fontWeight: 500,
+  fontSize: '14px',
+  color: '#80829F',
+  whiteSpace: 'nowrap',
+  marginBottom: '10px',
+  lineHeight: '16px'
 }))
-
-const InputBox = styled(Box)(() => ({
-  display: 'flex',
-  border: '1px solid #D4D7E2',
-  borderRadius: '8px',
-  height: '40px'
-}))
-
 const InputTypeTxt = styled(Typography)(() => ({
   fontFamily: 'Inter',
   fontStyle: 'normal',
   fontWeight: 500,
   fontSize: '14px',
   color: '#80829F',
-  whiteSpace: 'nowrap'
+  whiteSpace: 'nowrap',
+  lineHeight: '16px'
 }))
 const Title = styled(Typography)(() => ({
   fontFamily: 'Inter',
@@ -54,13 +51,6 @@ const Title = styled(Typography)(() => ({
   color: '#3F5170',
   whiteSpace: 'nowrap'
 }))
-
-const VerticalBar = styled(Box)(() => ({
-  width: '0px',
-  height: '19.5px',
-  border: '1px solid #D4D7E2'
-}))
-
 const InputHintStyle = styled(Typography)(() => ({
   fontFamily: 'Inter',
   fontStyle: 'normal',
@@ -69,6 +59,28 @@ const InputHintStyle = styled(Typography)(() => ({
   lineHeight: '20px',
   color: '#B5B7CF',
   whiteSpace: 'nowrap'
+}))
+
+const Row = styled(Box)(() => ({
+  width: '100%',
+  maxWidth: '866px',
+  display: 'flex',
+  justifyContent: 'space-between'
+}))
+
+const InputBox = styled(Box)(() => ({
+  boxSizing: 'border-box',
+  display: 'flex',
+  border: '1px solid #D4D7E2',
+  borderRadius: '8px',
+  height: '40px',
+  alignItems: 'center'
+}))
+
+const VerticalBar = styled(Box)(() => ({
+  width: '0px',
+  height: '19.5px',
+  border: '1px solid #D4D7E2'
 }))
 
 export default function CreateStepTwo() {
@@ -83,8 +95,15 @@ export default function CreateStepTwo() {
     lineHeight: '16px',
     color: '#3F5170'
   }
-  const [amountValue] = useState('108')
+  const [inputValue, setInputValue] = useState('108')
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
 
+  const [SwitchState, setSwitchState] = useState(false)
+  const SwitchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSwitchState(event.target.checked)
+  }
   return (
     <Box
       sx={{
@@ -102,11 +121,11 @@ export default function CreateStepTwo() {
         <Row sx={{ mt: '40px' }}>
           <Box>
             <InputTitle>Select Network</InputTitle>
-            <SelectComponent />
+            <SelectComponent width={413} />
           </Box>
           <Box>
             <InputTitle>Sale Token</InputTitle>
-            <SelectComponent />
+            <SelectComponent width={413} />
           </Box>
         </Row>
         <Row sx={{ mt: '20px' }}>
@@ -116,29 +135,29 @@ export default function CreateStepTwo() {
               <InputTitle>Balance: 200,000 RAI</InputTitle>
             </Row>
             <InputBox sx={{ width: '413px' }}>
-              <Input value={amountValue} style={inputStyle} />
-              <InputTypeTxt sx={{ padding: '10px', lineHeight: '20px' }}>RAI</InputTypeTxt>
+              <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
+              <InputTypeTxt sx={{ padding: '0 10px', lineHeight: '20px' }}>RAI</InputTypeTxt>
             </InputBox>
           </Box>
           <Box>
             <InputTitle>Receiving Token</InputTitle>
             <Box>
-              <SelectComponent />
+              <SelectComponent width={413} />
             </Box>
           </Box>
         </Row>
         <Box sx={{ mt: '20px' }}>
           <InputTitle>Price</InputTitle>
           <Title>{'1 RAI = 0.01 STPT    /   1 STPT = 100 RAI'}</Title>
-          <Row sx={{ mt: '20px' }}>
-            <InputTitle>Set sale price</InputTitle>
+          <Row sx={{ mt: '20px', mb: '10px' }}>
+            <InputTypeTxt sx={{ pt: '4px' }}>Set sale price</InputTypeTxt>
             <ToggleButtn />
           </Row>
           <InputBox>
-            <InputTypeTxt sx={{ padding: '12px 68px 0 17px ', lineHeight: '16px' }}>Unit price</InputTypeTxt>
-            <VerticalBar sx={{ mt: '12px' }} />
-            <Input value={amountValue} style={inputStyle} />
-            <InputHintStyle sx={{ padding: '10px 18px 10px 0' }}>STPT</InputHintStyle>
+            <InputTypeTxt sx={{ padding: '0 68px 0 17px ' }}>Unit price</InputTypeTxt>
+            <VerticalBar />
+            <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
+            <InputHintStyle sx={{ pr: '18px' }}>STPT</InputHintStyle>
           </InputBox>
         </Box>
         <Box sx={{ mt: '20px' }}>
@@ -152,56 +171,175 @@ export default function CreateStepTwo() {
               <Title>{'- 20%'}</Title>
             </Box>
           </Row>
-          <Row sx={{ mt: '24px' }}>
-            <InputTitle>Method of sales</InputTitle>
+          <Row sx={{ mt: '20px', mb: '10px' }}>
+            <InputTypeTxt sx={{ pt: '4px' }}>Method of sales</InputTypeTxt>
             <ToggleButtn />
           </Row>
           <InputBox>
             <Box sx={{ maxWidth: '289px', width: '100%' }}>
-              <InputTypeTxt sx={{ padding: '12px 0 12px 17px ', lineHeight: '16px' }}>One-time purchase</InputTypeTxt>
+              <InputTypeTxt sx={{ padding: '12px 0 12px 17px ' }}>One-time purchase</InputTypeTxt>
             </Box>
-            <VerticalBar sx={{ mt: '11.5px' }} />
+            <VerticalBar />
             <Row sx={{ maxWidth: '289px' }}>
-              <Input value={amountValue} style={inputStyle} />
+              <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
               <InputHintStyle sx={{ padding: '11px 20px 9px 0' }}>RAI</InputHintStyle>
             </Row>
-            <VerticalBar sx={{ mt: '11.5px' }} />
+            <VerticalBar />
             <Box sx={{ maxWidth: '288px' }}>
-              <InputHintStyle sx={{ padding: '13px 18px 11px 30px', lineHeight: '16px' }}>
-                {'Total share: 2,000.  200 STPT/share'}
-              </InputHintStyle>
+              <Input
+                value={''}
+                onChange={handleInputChange}
+                style={{
+                  height: '40px',
+                  width: '280px',
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                  fontFamily: 'Inter',
+                  fontStyle: 'normal',
+                  fontWeight: 500,
+                  fontSize: '14px',
+                  lineHeight: '16px',
+                  color: '#3F5170'
+                }}
+                placeholderSize="13px"
+                placeholder="Total share: 2,000.  200 STPT/share"
+              />
             </Box>
           </InputBox>
           <InputBox sx={{ mt: '10px' }}>
             <Box sx={{ maxWidth: '289px', width: '100%' }}>
-              <InputTypeTxt sx={{ padding: '12px 0 12px 17px ', lineHeight: '16px' }}>Purchase limit</InputTypeTxt>
+              <InputTypeTxt sx={{ padding: '12px 0 12px 17px ' }}>Purchase limit</InputTypeTxt>
             </Box>
 
-            <VerticalBar sx={{ mt: '11.5px' }} />
+            <VerticalBar />
             <Row sx={{ maxWidth: '285px', width: '100%' }}>
               <InputHintStyle sx={{ padding: '11px 0  9px 20px ' }}>Min</InputHintStyle>
-              <Input value={amountValue} style={inputStyle} />
+              <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
               <InputHintStyle sx={{ padding: '11px 20px 9px 0' }}>RAI</InputHintStyle>
             </Row>
             <VerticalBar
               sx={{
-                mt: '20px',
                 width: '10px',
-                border: '2px solid #D4D7E2',
+                border: '1px solid #D4D7E2',
                 height: '0'
               }}
             />
             <Row sx={{ maxWidth: '280px' }}>
               <InputHintStyle sx={{ padding: '11px 0  9px 20px ' }}>Max</InputHintStyle>
-              <Input value={amountValue} style={inputStyle} />
+              <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
               <InputHintStyle sx={{ padding: '11px 18px 9px 0' }}>RAI</InputHintStyle>
             </Row>
           </InputBox>
           <Box sx={{ mt: '20px', display: 'flex' }}>
-            <InputTitle>Whitelist</InputTitle>
+            <InputTitle sx={{ mr: '14px' }}>Whitelist</InputTitle>
+            <SwitchBtn checked={SwitchState} onChange={SwitchChange} />
           </Box>
+          <Row
+            sx={{
+              boxSizing: 'border-box',
+              maxwidth: '866px',
+              height: '82px',
+              background: '#F8FBFF',
+              border: ' 1px solid #D4D7E2',
+              borderRadius: '8px',
+              alignItems: 'center'
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                ml: '36px'
+              }}
+            >
+              <Typography variant="h6" lineHeight="20px" color="#0049C6">
+                0
+              </Typography>
+
+              <Typography variant="h6" lineHeight="20px" color="#3F5170">
+                &nbsp;{' address(s), Download this'}&nbsp;
+              </Typography>
+
+              <Link href="#" variant="h6" lineHeight="20px" color="#0049C6">
+                {' Reference template.'}
+              </Link>
+            </Box>
+            <Button
+              variant="outlined"
+              sx={{
+                boxSizing: 'border-box',
+                width: '200px',
+                height: '40px',
+                background: '#FFFFFF',
+                border: '1px solid #0049C6',
+                borderRadius: '8px',
+                color: '#0049C6',
+                mr: '24px'
+              }}
+            >
+              Upload file
+            </Button>
+          </Row>
         </Box>
-        <Box sx={{ mt: '30px', display: 'flex', justifyContent: 'space-between' }}>
+        <Box sx={{ mt: '20px' }}>
+          <InputTypeTxt sx={{ mb: '10px' }}>Event time</InputTypeTxt>
+          <Row>
+            <InputBox width="413px">
+              <Box sx={{ display: 'flex', alignItems: 'center', pl: '14px' }}>
+                <Timeicon />
+                <InputTypeTxt sx={{ pl: '8px', lineHeight: '20px' }}>Start time</InputTypeTxt>
+                <VerticalBar sx={{ ml: '46px' }} />
+              </Box>
+            </InputBox>
+            <VerticalBar
+              sx={{
+                mt: '20px',
+                width: '10px',
+                border: '1px solid #D4D7E2',
+                height: '0'
+              }}
+            />
+            <InputBox width="413px">
+              <Box sx={{ display: 'flex', alignItems: 'center', pl: '14px' }}>
+                <Timeicon />
+                <InputTypeTxt sx={{ pl: '8px', lineHeight: '20px' }}>End time</InputTypeTxt>
+                <VerticalBar sx={{ ml: '46px' }} />
+              </Box>
+            </InputBox>
+          </Row>
+        </Box>
+        <Box sx={{ mt: '20px' }}>
+          <InputTypeTxt sx={{ mb: '10px' }}>Tokens locked type</InputTypeTxt>
+          <SelectComponent width={866} />
+        </Box>
+        <Box sx={{ mt: '19px' }}>
+          <Row>
+            <Box>
+              <InputTypeTxt sx={{ mb: '10px' }}>Locked token ratio</InputTypeTxt>
+              <InputBox sx={{ width: '263px', pr: '10px' }}>
+                <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
+                <InputHintStyle>%</InputHintStyle>
+              </InputBox>
+            </Box>
+            <Box>
+              <InputTypeTxt sx={{ mb: '10px' }}>Locked days</InputTypeTxt>
+              <InputBox sx={{ width: '263px', pr: '10px' }}>
+                <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
+                <InputHintStyle>Days</InputHintStyle>
+              </InputBox>
+            </Box>
+            <Box>
+              <InputTypeTxt sx={{ mb: '10px' }}>Unlock period</InputTypeTxt>
+              <InputBox sx={{ width: '263px', pr: '10px' }}>
+                <Input value={inputValue} onChange={handleInputChange} style={inputStyle} />
+              </InputBox>
+            </Box>
+          </Row>
+        </Box>
+        <Box sx={{ mt: '20px' }}>
+          <Tablee />
+        </Box>
+        <Row sx={{ mt: '30px', mb: '50px' }}>
           <Button sx={{ width: '200px', height: '40px', border: '1px solid #0049C6' }}>BACK</Button>
           <Button
             variant="contained"
@@ -215,27 +353,49 @@ export default function CreateStepTwo() {
           >
             Public
           </Button>
-        </Box>
+        </Row>
       </Box>
     </Box>
   )
 }
-function SelectComponent() {
-  const selectStyle = {
-    height: '40px',
-    width: '413px'
-    // border: '1px solid #D4D7E2'
+function SelectComponent({ width }: { width: number }) {
+  const CustomSelect = styled(Select)(() => ({
+    height: 40,
+    width: width,
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '14px',
+    lineHeight: '20px',
+    color: '#3F5170',
+    '& fieldset': {
+      border: '1px solid #D4D7E2'
+    }
+  }))
+  const optionStyle = {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '14px',
+    lineHeight: '20px',
+    color: '#3F5170'
   }
   const [selectedValue, setselectedValue] = useState('option1')
-  const handleChange = (event: SelectChangeEvent) => {
-    setselectedValue(event.target.value)
+  const handleChange = (event: SelectChangeEvent<unknown>) => {
+    setselectedValue(event.target.value as string)
   }
   return (
-    <Select style={selectStyle} labelId="select-label" id="select" value={selectedValue} onChange={handleChange}>
-      <MenuItem value="option1">ABC</MenuItem>
-      <MenuItem value="option2">ABC</MenuItem>
-      <MenuItem value="option3">ABC</MenuItem>
-    </Select>
+    <CustomSelect labelId="select-label" id="select" value={selectedValue} onChange={handleChange}>
+      <MenuItem value="option1" sx={optionStyle}>
+        ABC
+      </MenuItem>
+      <MenuItem value="option2" sx={optionStyle}>
+        ABC
+      </MenuItem>
+      <MenuItem value="option3" sx={optionStyle}>
+        ABC
+      </MenuItem>
+    </CustomSelect>
   )
 }
 function ToggleButtn() {
@@ -263,9 +423,9 @@ function ToggleButtn() {
     fontWeight: 500,
     fontSize: '12px'
   }
-  const [alignment, setAlignment] = useState('Unit')
+  const [alignment, setAlignment] = useState(true)
 
-  const handleChange = (event: React.MouseEvent<HTMLElement>, newAlignment: string) => {
+  const handleChange = (event: React.MouseEvent<HTMLElement>, newAlignment: boolean) => {
     setAlignment(newAlignment)
   }
   return (
@@ -278,24 +438,100 @@ function ToggleButtn() {
         borderRadius: ' 48px'
       }}
       color="primary"
-      value={alignment}
       exclusive
       onChange={handleChange}
     >
       <ToggleButton
         sx={{ boxSizing: 'border-box' }}
-        value="Unit"
-        style={alignment == 'Unit' ? toggleTrueStyle : toggleButtonStyle}
+        value={true}
+        style={alignment ? toggleTrueStyle : toggleButtonStyle}
       >
         Unit price
       </ToggleButton>
       <ToggleButton
         sx={{ boxSizing: 'border-box' }}
-        value="Package"
-        style={alignment == 'Package' ? toggleTrueStyle : toggleButtonStyle}
+        value={false}
+        style={!alignment ? toggleTrueStyle : toggleButtonStyle}
       >
         Package price
       </ToggleButton>
     </ToggleButtonGroup>
+  )
+}
+
+function Tablee() {
+  const StyledTableCell = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+      padding: 0,
+      height: '31px',
+      backgroundColor: '#F8FBFF',
+      fontFamily: 'Inter',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '14px',
+      lineHeight: '18px',
+      textAlign: 'center',
+      color: '#3F5170',
+      border: 'none',
+      borderLeft: '1px solid #D4D7E2'
+    },
+    [`&.${tableCellClasses.head}.firstColumn`]: {
+      borderLeft: 'none'
+    },
+    [`&.${tableCellClasses.body}`]: {
+      padding: 0,
+      height: '30px',
+      fontFamily: 'Inter',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '12px',
+      lineHeight: '18px',
+      textAlign: 'center',
+      color: '#3F5170'
+    }
+  }))
+
+  const StyledTableRow = styled(TableRow)(() => ({
+    '& td': {
+      borderTop: '1px solid #D4D7E2',
+      borderLeft: '1px solid #D4D7E2',
+      height: '31px'
+    },
+    '& td:first-of-type': {
+      borderLeft: 'none'
+    },
+    '&:last-child td': {
+      borderBottom: 'none'
+    }
+  }))
+
+  const rows = [
+    { Number: 1, days: 10, quota: '10%' },
+    { Number: 2, days: 10, quota: '10%' },
+    { Number: 3, days: 10, quota: '10%' },
+    { Number: 4, days: 10, quota: '10%' }
+  ]
+
+  return (
+    <TableContainer sx={{ border: '1px solid #D4D7E2', borderRadius: '8px' }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell className="firstColumn">Number</StyledTableCell>
+            <StyledTableCell>Number of days</StyledTableCell>
+            <StyledTableCell>Release quota</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <StyledTableRow key={row.Number}>
+              <StyledTableCell>{row.Number}</StyledTableCell>
+              <StyledTableCell>{row.days}</StyledTableCell>
+              <StyledTableCell>{row.quota}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
--- src/pages/Sales/index.tsx
+++ src/pages/Sales/index.tsx
@@ -1,12 +1,13 @@
 // import CreateStepOne from './CreateStepOne'
+import { Box } from '@mui/material'
 import CreateStepTwo from './CreateStepTwo'
 
 export default function Index() {
   return (
-    <>
+    <Box>
       {/* <CreateStepOne /> */}
 
       <CreateStepTwo />
-    </>
+    </Box>
   )
 }
